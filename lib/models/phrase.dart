98a1170404421ed58aa021bdd10d2b537d1088f9
nlmb372 <ilikethisone3456@gmail.com>
2026-02-22 18:47:55 -0600

    Create phrase.dart

---
+++ lib/models/phrase.dart
@@ -0,0 +1,11 @@
+class Phrase {
+  final String english;
+  final String spanish;
+  final String category;
+
+  Phrase({
+    required this.english,
+    required this.spanish,
+    required this.category,
+  });
+}
